b7dfa5d752ee4516928866802f7285e9317168c0
Shugo USHIO <ushio.s@gmail.com>
2023-02-23 10:06:11 +0900

    Add tick sample

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Octane\Facades\Octane;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Octane::tick('send-stats', function() {
+            $stats = app('Swoole\Http\Server')->stats();
+            printf("workers_total: %d, workers_idle: %d, task_workers_total: %d, task_workers_idle: %d",
+                $stats['workers_total'],
+                $stats['workers_idle'],
+                $stats['task_workers_total'],
+                $stats['task_workers_idle']
+            );
+        })
+        ->seconds(1)
+        ->immediate();
+
+        Octane::tick('using-task-worker', function() {
+            sleep(10);
+        })
+        ->seconds(5)
+        ->immediate();
     }
 }
